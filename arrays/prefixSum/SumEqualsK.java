8bdb1546f43095f08b67ee2d47aaa9c70c12a10c
Omar Shawky <ipowerfuliii@gmail.com>
2023-09-26 23:42:16 +0300

    Add: SumEqualsK

---
+++ arrays/prefixSum/SumEqualsK.java
@@ -0,0 +1,69 @@
+package arrays.prefixSum;
+
+import java.util.HashMap;
+
+/**
+ * Find subarrays of nums that give sum k
+ * Problem link: https://leetcode.com/problems/subarray-sum-equals-k/
+ */
+public class SumEqualsK {
+    /**
+     * return number of subarrays that has sum of k
+     *
+     * @param nums
+     * @param k
+     * @return sum
+     */
+
+    // Brute Force, infeasible due to constrains
+    public int subarraySum1(int[] nums, int k) {
+        int sum = 0;
+        //For each window size, start with window = len - 1
+        for (int w = nums.length; w > 0; w--) {
+            // for each window index i
+            for (int i = 0; (i < nums.length) && (i + w - 1 < nums.length); i++) {
+                int tempWS = 0; // Temp window sum
+                for (int j = i; j < i + w; j++) tempWS += nums[j];
+                if (tempWS == k) sum++;
+            }
+        }
+        return sum;
+    }
+
+    // Enhanced brute force
+    public int subarraySum2(int[] nums, int k) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int tempWS = 0;
+            for (int j = i; j < nums.length; j++) {
+                tempWS += nums[j];
+                if (tempWS == k) sum++;
+            }
+        }
+        return sum;
+    }
+
+    // Solution Link: <a href="https://leetcode.com/problems/subarray-sum-equals-k/solutions/803317/java-solution-with-detailed-explanation/">...</a>
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i]; // Create prefix sum array
+            if (map.containsKey(sum - k)) count += map.get(sum - k); // check
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+
+
+    public static void test() {
+        SumEqualsK s = new SumEqualsK();
+        assert s.subarraySum(new int[]{1, 1, 1}, 2) == 2;
+        assert s.subarraySum(new int[]{1, 2, 3}, 3) == 2;
+        assert s.subarraySum(new int[]{1, -1, 0}, 0) == 3;
+        assert s.subarraySum(new int[]{1, -1, 0, 4, 5, -5}, 0) == 4;
+    }
+}
