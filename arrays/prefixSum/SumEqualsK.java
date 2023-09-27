f17f56466c348fe96729c1978c2e1d77c80ab971
Omar Shawky <ipowerfuliii@gmail.com>
2023-09-27 18:37:55 +0300

    Add: other solutions to SumEqualsK

--- arrays/prefixSum/SumEqualsK.java
+++ arrays/prefixSum/SumEqualsK.java
@@ -44,20 +44,59 @@ public class SumEqualsK {
     }
 
     // Solution Link: <a href="https://leetcode.com/problems/subarray-sum-equals-k/solutions/803317/java-solution-with-detailed-explanation/">...</a>
-    public int subarraySum(int[] nums, int k) {
+    public int subarraySum3(int[] nums, int k) {
+        if (nums.length == 0) return 0;
         int count = 0, sum = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
         map.put(0, 1);
 
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i]; // Create prefix sum array
-            if (map.containsKey(sum - k)) count += map.get(sum - k); // check
+
+            // Cumulative addition; Check if same elem resulted in multiple "k"s
+            if (map.containsKey(sum - k)) count += map.get(sum - k);
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
 
+    // Modified version of the one below
+    public int subarraySum4(int[] nums, int k) {
+        if (nums.length == 0) return 0;
+        int count = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
 
+        // Create prefix sum array
+        int[] prefix = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) prefix[i + 1] = prefix[i] + nums[i];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(prefix[i + 1] - k)) count += map.get(prefix[i + 1] - k);
+            map.put(prefix[i + 1], map.getOrDefault(prefix[i + 1], 0) + 1);
+        }
+        return count;
+    }
+
+    // Fastest with 99.89%. Code explanation is in code 3
+    public int subarraySum(int[] nums, int k) {
+        if (nums.length == 0) return 0;
+        int count = 0;
+
+        // Create prefix sum array
+        int[] prefix = new int[nums.length];
+        prefix[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) prefix[i] = prefix[i - 1] + nums[i];
+        for (int i = 0; i < nums.length; i++) if (prefix[i] == k) count++;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(prefix[i] - k)) count += map.get(prefix[i] - k);
+            map.put(prefix[i], map.getOrDefault(prefix[i], 0) + 1);
+        }
+        return count;
+    }
 
     public static void test() {
         SumEqualsK s = new SumEqualsK();
@@ -65,5 +104,6 @@ public class SumEqualsK {
         assert s.subarraySum(new int[]{1, 2, 3}, 3) == 2;
         assert s.subarraySum(new int[]{1, -1, 0}, 0) == 3;
         assert s.subarraySum(new int[]{1, -1, 0, 4, 5, -5}, 0) == 4;
+        assert s.subarraySum(new int[]{1}, 1) == 1;
     }
 }
