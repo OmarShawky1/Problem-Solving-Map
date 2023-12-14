afe12712127a724d353b27cb81f8033cb9dc5e84
Omar Shawky <ipowerfuliii@gmail.com>
2023-12-14 18:01:48 +0200

    Add: Valid Parentheses

---
+++ arrays/stack/ValidParentheses.java
@@ -0,0 +1,37 @@
+package arrays.stack;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    // Most Maintainable code, use array instead of stack if you want it faster.
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') return false;
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{') return false;
+                    break;
+                default:
+                    stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+    public static void test() {
+        ValidParentheses v = new ValidParentheses();
+        assert v.isValid("()");
+        assert v.isValid("()[]{}");
+        assert !v.isValid("(]");
+        assert v.isValid("(([]){})");
+    }
+}
