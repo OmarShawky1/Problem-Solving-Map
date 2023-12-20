c2aa9c9ee06a789f635f12ded3e16944bd1e0998
Omar Shawky <ipowerfuliii@gmail.com>
2023-12-20 10:50:17 +0200

    Add: Binary Search: Princeton Implementation

--- arrays/sortingAndSearching/binarySearch/BinarySearch.java
+++ arrays/sortingAndSearching/binarySearch/BinarySearch.java
@@ -1,7 +1,21 @@
 package arrays.sortingAndSearching.binarySearch;
 
 public class BinarySearch {
+
+    // Princeton Implementation
     public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (target < nums[mid]) right = mid - 1;
+            else if (target > nums[mid]) left = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    public int search1(int[] nums, int target) {
         int left = 0, right = nums.length - 1, mid;
 
         while (left < right) {
@@ -10,7 +24,6 @@ public class BinarySearch {
             else if (nums[mid] > target) right = mid;
             else left = mid + 1;
         }
-
         return -1;
     }
 
@@ -18,5 +31,6 @@ public class BinarySearch {
         BinarySearch b = new BinarySearch();
         assert b.search(new int[]{-1, 0, 3, 5, 9, 12}, 9) == 4;
         assert b.search(new int[]{-1, 0, 3, 5, 9, 12}, 2) == -1;
+        assert b.search(new int[]{1, 3}, 1) == 0;
     }
 }
