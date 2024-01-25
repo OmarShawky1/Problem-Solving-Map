f8c74942f656883e7db891d1a770f1d6f53aa8eb
Omar Shawky <ipowerfuliii@gmail.com>
2024-01-25 14:41:45 +0200

    Fix: minor

--- linkedList/ReverseList.java
+++ linkedList/ReverseList.java
@@ -5,11 +5,13 @@ public class ReverseList {
     public ListNode reverseList(ListNode head) {
         ListNode prev = null, curr = head;
         while (curr != null) {
-            // store
-            ListNode next = curr.next;
+            ListNode next = curr.next; // store
+
+            //reverse
             curr.next = prev;
             prev = curr;
-            curr = next;
+
+            curr = next; // next
         }
 
         return prev;
