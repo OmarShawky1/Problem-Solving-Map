f92c3379253cef92a7309df967fb054db34fbe78
Omar Shawky <ipowerfuliii@gmail.com>
2024-02-06 22:36:47 +0200

    Add: Min Window: Added array technique

--- arrays/slidingWindow/MinWindow.java
+++ arrays/slidingWindow/MinWindow.java
@@ -4,6 +4,62 @@ import java.util.*;
 
 public class MinWindow {
 
+    // Fastest and very maintainable
+    public String minWindow1(String s, String t) {
+        // Check for null or empty strings
+        if (s == null || t == null || t.isEmpty() || s.length() < t.length()) return "";
+
+        // Frequency array for t
+        int[] freqT = new int[26 * 2 + 7]; // 26 are from 'a' to 'z', times 2 for big chars, 7 is letters between 'Z' & 'a'
+        for (char c : t.toCharArray()) freqT[c - 'A']++;
+        int notInT = Integer.MIN_VALUE; // Value that represents that character not in 't'
+        for (int i = 0; i < freqT.length; i++)
+            if (freqT[i] == 0) freqT[i] = notInT; // mark character not in array
+
+        // Pointers to represent the window
+        int start = 0;
+        // Minimum length of the window
+        int minLength = Integer.MAX_VALUE;
+        // Start index of the minimum window substring
+        int minStart = 0;
+        // Number of characters from 't' that are still needed to form a valid window
+        int windowSize = t.length();
+
+        // Move resizing sliding window right
+        // Slide the window through the string 's'
+        for (int end = 0; end < s.length(); end++) {
+            // Current character in the window
+            char currentChar = s.charAt(end);
+
+            // If current character is in t
+            // Update the count for the current character in freqA
+            if (freqT[currentChar - 'A'] != notInT) {
+                freqT[currentChar - 'A']--;
+                // If the count becomes non-negative, decrement the requiredChars count
+                if (freqT[currentChar - 'A'] >= 0) windowSize--;
+            }
+
+            while (windowSize == 0) {
+                // Update the minimum window length and start index
+                if (end - start + 1 < minLength) {
+                    minLength = end - start + 1;
+                    minStart = start;
+                }
+                // shift window to right
+                // Character at the left end of the window
+                char startChar = s.charAt(start);
+                // Update the map and requiredChars count for the left character
+                if (freqT[startChar - 'A'] != notInT) {
+                    freqT[startChar - 'A']++;
+                    if (freqT[startChar - 'A'] > 0) windowSize++;
+                }
+                start++;
+            }
+        }
+
+        return minLength != Integer.MAX_VALUE ? s.substring(minStart, minStart + minLength) : "";
+    }
+
     // Most maintainable
     public String minWindow(String s, String t) {
         // Check for null or empty strings
@@ -69,5 +125,6 @@ public class MinWindow {
         System.out.println(m.minWindow("ADOBECODEBANC", "ABC"));
         assert m.minWindow("ADOBECODEBANC", "ABC").equals("BANC");
         assert m.minWindow("a", "aa").equals("");
+        assert m.minWindow("cabwefgewcwaefgcf", "cae").equals("cwae");
     }
 }
