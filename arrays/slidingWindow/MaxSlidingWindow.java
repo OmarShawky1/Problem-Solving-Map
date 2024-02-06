941009becdcf9b69e23ebc536f666493ecee365e
Omar Shawky <ipowerfuliii@gmail.com>
2024-02-06 21:54:43 +0200

    Add: Max Sliding: TLE sol

--- arrays/slidingWindow/MaxSlidingWindow.java
+++ arrays/slidingWindow/MaxSlidingWindow.java
@@ -3,8 +3,26 @@ package arrays.slidingWindow;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.PriorityQueue;
 
 public class MaxSlidingWindow {
+    // TLE
+    public int[] maxSlidingWindow2(int[] nums, int k) {
+        int[] ans = new int[nums.length - k + 1];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < k; i++) pq.add(-nums[i]);
+        ans[0] = -pq.peek();
+
+        for (int i = k; i < nums.length; i++) {
+            pq.remove(-nums[i - k]);
+            pq.add(-nums[i]);
+            ans[i - k + 1] = -pq.peek();
+        }
+
+        return ans;
+    }
+
     public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length == 0) return new int[0];
 
