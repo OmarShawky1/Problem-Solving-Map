ebd9c25a5b9810b2b8889de69c10f300741d48f3
Omar Shawky <ipowerfuliii@gmail.com>
2024-02-07 15:49:37 +0200

    Mod: ReorderList: Minor edits and backtracking sol addition

--- linkedList/ReorderList.java
+++ linkedList/ReorderList.java
@@ -2,10 +2,11 @@ package linkedList;
 
 public class ReorderList {
 
+    // Most Maintainable
+    // Hare Tortoise Algorithm
     public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
+        // Unnecessary check, can be removed
+        if (head == null || head.next == null) return;
         // Find middle
         ListNode middle = findMiddle(head);
 
@@ -50,6 +51,7 @@ public class ReorderList {
         ListNode slow = head;
         ListNode fast = head;
 
+        // fast.next.next to reach element before middle as to make it point to null
         while (fast != null && fast.next != null && fast.next.next != null) {
             slow = slow.next;
             fast = fast.next.next;
@@ -64,6 +66,37 @@ public class ReorderList {
 
         ListNode(int val) {
             this.val = val;
+        }
+    }
+
+    // Back tracking solution: https://chat.openai.com/share/d4bdc04e-49ce-4f37-951d-b7fc056faba8
+    // Fastest
+    class Solution {
+        public static void reorderList(ListNode head) {
+            if (head.next == null) {
+                return;
+            }
+            reorderList2(head, head.next);
+        }
+
+        public static ListNode reorderList2(ListNode head, ListNode curr) {
+            ListNode temp;
+            if (curr.next != null) {
+                temp = reorderList2(head, curr.next);
+            } else {
+                temp = head;
+            }
+            if(temp == null) {
+                return null;
+            }
+            if (temp == curr  || temp.next == curr) {
+                curr.next=null;
+                return null;
+            }
+
+            curr.next = temp.next;
+            temp.next = curr;
+            return curr.next;
         }
     }
 
