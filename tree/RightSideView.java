0492196722af3a2abc407a30dc75c11f959ba22d
Omar Shawky <ipowerfuliii@gmail.com>
2024-02-13 17:00:11 +0200

    Add: RightSideView

---
+++ tree/RightSideView.java
@@ -0,0 +1,52 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        List<List<Integer>> preAns = levelOrder(root);
+        for (List<Integer> loi : preAns) {
+            ans.add(loi.get(loi.size() - 1));
+        }
+
+        return ans;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        //Using queues
+        List<List<Integer>> ls = new ArrayList<>();
+        preOrder(root, 0, ls);
+        return ls;
+    }
+
+    public static void preOrder(TreeNode root, int level, List<List<Integer>> ls) {
+        if (root == null) return;
+        if (ls.size() == level) {
+            List<Integer> li = new ArrayList<>();
+            li.add(root.val);
+            ls.add(li);
+        } else ls.get(level).add(root.val); // If level is already available, just add to its list.
+        preOrder(root.left, level + 1, ls);
+        preOrder(root.right, level + 1, ls);
+    }
+
+    public List<Integer> rightSideView2(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inversePreOrder(root, 0, ans);
+        return ans;
+    }
+
+    public void inversePreOrder(TreeNode node, int level, List<Integer> ans) {
+        if (node != null) {
+            if (level == ans.size()) ans.add(node.val);
+            inversePreOrder(node.right, level + 1, ans);
+            inversePreOrder(node.left, level + 1, ans);
+        }
+    }
+
+    public static void test() {
+
+    }
+}
